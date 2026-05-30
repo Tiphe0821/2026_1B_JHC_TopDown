3b611529e5ce9c88ba98eec86f92a65b58a260cc
Tiphe0821 <namucho24@gmail.com>
2026-05-30 13:37:46 +0900

    [feat] 플레이어 시선 처리 변경
    플레이어의 스프라이트 변경 기준을 플레이어의 움직임 기준이 아닌 마우스 위치 기반으로 변경
    (테스트 완료.)
    
    다음 구현 예정
    - 플레이어 달리기 구현
    - 플레이어 무기 홀드 구현 (쿼터니언 기반 로테이션 & 스케일값 조정으로 OnLook에서 마우스 따라가도록 설정해보기)
    
    필요 리소스
    - 플레이어 애니메이션에 활용할 플레이어 리소스 (적합 리소스 없음)
    - 리소스 우선순위 : 플레이어 리소스
    - 타일맵 -> 후보군 2개 존재

--- 2026_TopDown_Lecture/Assets/Scripts/PlayerController.cs
+++ 2026_TopDown_Lecture/Assets/Scripts/PlayerController.cs
@@ -4,25 +4,30 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
-    public Sprite[] spriteUp;
-    public Sprite[] spriteDown;
-    public Sprite[] spriteLeft;
-    public Sprite[] spriteRight;
+    public Sprite[] spriteRightUp;
+    public Sprite[] spriteRightDown;
+    public Sprite[] spriteLeftUp;
+    public Sprite[] spriteLeftDown;
     public float frameTime = 0.15f;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
-    private Vector2 input;
+    private Vector2 moveInput;
+    private Vector2 worldMouseInput;
     private Vector2 velocity;
     private Sprite[] currentSprites;
     private int frameIndex = 0;
     private float timer = 0f;
 
+    private Camera mainCamera;
+
     private void Awake()
     {
+        mainCamera = Camera.main;
+
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
 
-        currentSprites = spriteDown;
+        currentSprites = spriteRightDown;
         sr.sprite = currentSprites[0];
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -33,30 +38,65 @@ public class PlayerController : MonoBehaviour
 
     public void OnMove(InputValue value)
     {
-        input = value.Get<Vector2>();
-        velocity = input.normalized * moveSpeed;
-        if (input.magnitude > 0.01f)
+        moveInput = value.Get<Vector2>();
+        velocity = moveInput.normalized * moveSpeed;
+
+        /*
+        if (moveInput.magnitude > 0.01f)
         {
-            if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+            if (Mathf.Abs(moveInput.x) > Mathf.Abs(moveInput.y))
             {
-                if (input.x > 0)
-                    ChangeSprites(spriteRight);
+                if (moveInput.x > 0)
+                    ChangeSprites(spriteLeftDown);
                 else
-                    ChangeSprites(spriteLeft);
+                    ChangeSprites(spriteLeftUp);
             }
             else
             {
-                if (input.y > 0)
-                    ChangeSprites(spriteUp);
+                if (moveInput.y > 0)
+                    ChangeSprites(spritRightUp);
                 else
-                    ChangeSprites(spriteDown);
+                    ChangeSprites(spriteRightDown);
             }
         }
+        */
+
     }
+
+    private void OnLook(InputValue value)   // 마우스가 움직일 때 호출됨 -> 사용해보자. (Delta값 받아오는걸 position 값 받아오게 바꾼 뒤 월드포지션으로 변환)
+    {
+        worldMouseInput = mainCamera.ScreenToWorldPoint(value.Get<Vector2>());
+
+        // 여기서 플레이어 보는 방향 정해주기
+        // 세피리아 따라할거라면 제대로.
+        // 세피리아에서 보는 방향 : 
+        // 좌-상 / 우-상
+        // 좌-하 / 우-하 (상하좌우 대신 무조건 왼쪽 또는 오른쪽을 본다)
+        // 월드 포지션을 받아오도록 했으니 플레이어 포지션도 받아와야겠지?
+        // 이게 플레이어 컨트롤 스크립트니까 그냥 transform.position 받아오자.
+
+        if(worldMouseInput.x > this.transform.position.x)       // 위랑 다르게 예외처리 따로 안하는 이유. -> 굳이? 마우스 포지션 + 플레이어 캐릭터 포지션 기반이라 예외처리가 필요 없을 것 같다
+        {
+            if(worldMouseInput.y > this.transform.position.y)
+                ChangeSprites(spriteRightUp);
+            else
+                ChangeSprites(spriteRightDown);
+        }
+        else
+        {
+            if (worldMouseInput.y > this.transform.position.y)
+                ChangeSprites(spriteLeftUp);
+            else
+                ChangeSprites(spriteLeftDown);
+        }
+
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if(input.sqrMagnitude <= 0.01f)
+        if(moveInput.sqrMagnitude <= 0.01f)
         {
             frameIndex = 0;
             sr.sprite = currentSprites[frameIndex];
